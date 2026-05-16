ac99694b1d4888bd6065513fd81630ea8141ebf3
mahir2625 <mahirxo26@gmail.com>
2026-05-16 22:42:09 +0600

    Update EventController.php
    added organiser event management actions

--- controllers/EventController.php
+++ controllers/EventController.php
@@ -28,5 +28,22 @@ class EventController extends BaseController
         $this->view('event/detail', ['event' => $event]);
     }
 
+    public function mine(): void
+    {
+        require_role('organiser');
+        $user = current_user();
+        $db = connect_db();
+        $events = Event::forOrganiser($db, $user['id']);
+        $this->view('event/mine', ['events' => $events]);
+    }
+
+    public function create(): void
+    {
+        require_role('organiser');
+        $db = connect_db();
+        $categories = Category::all($db);
+        $this->view('event/form', ['categories' => $categories, 'event' => null]);
+    }
+
     
 }
